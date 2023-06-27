319e715b5375cbe7749c17a9e8399a0a9228eeb4
chkharto <128228041+chkharto@users.noreply.github.com>
2023-06-27 16:59:16 +0400

    Add files via upload

---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/header";
+import Meme from "../components/meme";
+
+export default function Home() {
+
+  return (
+    <div className="container">
+      <Header />
+      <Meme />
+    </div>
+  );
+}
